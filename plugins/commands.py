ee2f15434c18cdcdf1d7aac3571fa8db523c326e
monitg <142651228+monitg@users.noreply.github.com>
2024-04-08 19:28:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,7 +15,7 @@ from bot import *
 logger = logging.getLogger(__name__)
 
 user_commands = ["set_api", "header", "footer", "username", "banner_image", "me"]
-avl_web = ["GreyMatterslinks.in",]
+avl_web = ["instantearn.in",]
 
 avl_web1 = "".join(f"- {i}\n" for i in avl_web)
 
